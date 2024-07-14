0ea65544256b1d7edc8928c772e0146cff0d5cf1
CRN91 <ak824@exeter.ac.uk>
2024-07-14 12:26:09 +0100

    Functions including; convert from axial to oddr, get neighbouring hex, create hex rings, create hex spirals, distance between hexes

---
+++ hex.gd
@@ -0,0 +1,39 @@
+# The 6 direction vectors for cube coordinates
+var cube_direction_vectors = [
+	Vector3i(+1, 0, -1), Vector3i(+1, -1, 0), Vector3i(0, -1, +1), 
+	Vector3i(-1, 0, +1), Vector3i(-1, +1, 0), Vector3i(0, +1, -1), 
+]
+
+# Converts axial to odd right offset coordinates
+func axial_to_oddr(hex):
+	var col = hex.x + (hex.y - (hex.y&1)) / 2
+	var row = hex.y
+	return Vector2i(col, row)
+
+# Returns the distance between two cube coordinates
+func cube_distance(a, b):
+	var vec = a - b
+	return max(abs(vec.x), abs(vec.y), abs(vec.z)) 
+
+# Returns the neighbouring hex in a given direction
+func cube_neighbor(cube, direction):
+	return cube + cube_direction_vectors[direction]
+
+# Returns a list of cube coordinates that make up a ring of hexagons
+# of radius distance away from the center
+func cube_ring(center, radius):
+	var results = []
+	var hex = center + cube_direction_vectors[4] * radius
+
+	for i in range(6):
+		for j in range(radius):
+			results.append(hex)
+			hex = cube_neighbor(hex, i)
+	return results
+
+# Returns a list of cube coordinates that make up a spiral hexagons
+func cube_spiral(center, radius):
+	var results = [center]
+	for k in range(1,radius):
+		results = results + cube_ring(center, k)
+	return results
